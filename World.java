1df2d1611515909af113eedba4545e8c831fa1be
RaviKumar-Ganti <ravikumarganti@yahoo.com>
2020-08-29 17:24:15 +0530

    this is updated with print statement

--- World.java
+++ World.java
@@ -4,6 +4,11 @@ public class World{
 public static void main(String[] args){
 System.out.println("This is a java prog");
 
+for(int i=0; i<10; i++){
+System.out.println("this is the logic"+i);
+
+}
+
 }
 
 }
