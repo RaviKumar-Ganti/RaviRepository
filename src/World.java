185003eecbeaebf3033ca05a2c0ef9243fe31f97
RaviKumar-Ganti <ravikumarganti@yahoo.com>
2020-09-27 16:45:50 +0530

    testing the git

---
+++ src/World.java
@@ -0,0 +1,15 @@
+public class World{
+
+
+public static void main(String[] args){
+System.out.println("This is a java prog");
+System.out.println("Welcom to GIT");
+System.out.println("Welcom to GITssss");
+for(int i=0; i<10; i++){
+System.out.println("this is the logic"+i);
+
+}
+
+}
+
+}
